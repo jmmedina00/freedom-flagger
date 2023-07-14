c6dba2abf930f2ffc6c05de162b060ae2a04d84e
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2023-07-14 17:20:20 +0200

    chore(config): add icon radio option component

---
+++ src/components/config/options/IconRadioOption.spec.js
@@ -0,0 +1,76 @@
+import { fireEvent, render } from '@testing-library/vue';
+import { describe, expect, test } from 'vitest';
+import IconRadioOption from './IconRadioOption.vue';
+import { ref } from 'vue';
+
+describe('IconRadioOption', () => {
+  const generate = (props) =>
+    render(IconRadioOption, {
+      props,
+      global: {
+        mocks: { $t: (foo) => foo },
+        stubs: {
+          DiscreteIcon: {
+            props: ['icon'],
+            template: '<span>{{icon}} - </span>',
+          },
+        },
+      },
+    });
+
+  const useUpdatableModel = (initialValue = '') => {
+    const reference = ref(initialValue);
+    const props = {
+      modelValue: reference.value,
+      'onUpdate:modelValue': (e) => {
+        reference.value = e;
+      },
+    };
+
+    return { reference, props };
+  };
+
+  test('should be checked if provided v-model matches value', async () => {
+    const { props } = useUpdatableModel('foo');
+
+    const { findByLabelText } = generate({
+      ...props,
+      icon: 'bar',
+      label: 'baz',
+      value: 'foo',
+    });
+
+    const radio = await findByLabelText('bar - baz'); // Asserts icon and
+    expect(radio.checked).toBeTruthy();
+  });
+
+  test('should not be checked if provided v-model does not match value', async () => {
+    const { props } = useUpdatableModel('foo');
+
+    const { findByLabelText } = generate({
+      ...props,
+      icon: 'bar',
+      label: 'baz',
+      value: 're',
+    });
+
+    const radio = await findByLabelText('bar - baz');
+    expect(radio.checked).toBeFalsy();
+  });
+
+  test('should be able to be checked and update v-model accordingly', async () => {
+    const { reference, props } = useUpdatableModel('foo');
+
+    const { findByLabelText } = generate({
+      ...props,
+      icon: 'bar',
+      label: 'baz',
+      value: 're',
+    });
+
+    const radio = await findByLabelText('bar - baz');
+    await fireEvent.click(radio);
+
+    expect(reference.value).toEqual('re');
+  });
+});
---
+++ src/components/config/options/IconRadioOption.vue
@@ -0,0 +1,67 @@
+<script setup>
+  import { computed } from 'vue';
+  import DiscreteIcon from '../../shared/DiscreteIcon.vue';
+
+  const props = defineProps({
+    id: { type: String, default: 'temp' },
+    icon: String,
+    label: String,
+    value: String,
+    modelValue: String,
+  });
+  const emit = defineEmits(['update:modelValue']);
+  const currentValue = computed({
+    get: () => props.modelValue,
+    set: (value) => {
+      emit('update:modelValue', value);
+    },
+  });
+</script>
+
+<template>
+  <input :id="id" type="radio" :value="value" v-model="currentValue" />
+  <label
+    class="option-display is-inline-flex is-flex-direction-column"
+    role="button"
+    :for="id"
+    tabindex="0"
+    :aria-label="$t(label)"
+  >
+    <div class="icon-holder is-flex">
+      <DiscreteIcon :icon="icon" size="48" />
+    </div>
+
+    <span>{{ $t(label) }}</span>
+  </label>
+</template>
+
+<style scoped lang="scss">
+  @use 'sass:color';
+  @import '@app/style';
+
+  .option-display {
+    text-align: center;
+    cursor: pointer;
+  }
+
+  .icon-holder {
+    background: color.change($primary, $lightness: 75%);
+    border-radius: 16px;
+    padding: 8px;
+  }
+
+  input[type='radio'] {
+    display: none;
+
+    &:checked + .option-display {
+      font-weight: bold;
+      .icon-holder {
+        background: $link;
+
+        .icon {
+          color: white;
+        }
+      }
+    }
+  }
+</style>
