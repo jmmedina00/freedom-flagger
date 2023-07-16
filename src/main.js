886836af0b7e7ec886bdeecb62a09f7d5738f3a9
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2023-07-16 14:06:55 +0200

    chore: provide some proper default value for the number

--- src/main.js
+++ src/main.js
@@ -25,7 +25,15 @@ import { NOTIFICATION, NUMBER_BYTES } from './state';
 
   app.use(i18n);
   app.use(provideDefault(NOTIFICATION, null));
-  app.use(provideDefault(NUMBER_BYTES, [0]));
+  app.use(
+    provideDefault(
+      NUMBER_BYTES,
+      [
+        100, 105, 115, 32, 105, 115, 32, 97, 32, 108, 111, 116, 32, 111, 102,
+        32, 102, 114, 101, 101, 100, 111, 109,
+      ] // dis is a lot of freedom
+    )
+  );
   app.use(config);
   app.mount('#app');
 })();
