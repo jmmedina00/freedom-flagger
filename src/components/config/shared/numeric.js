a421057a9752be8b849d770acafc5cd514202a0f
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2023-07-19 15:38:23 +0200

    chore(config): remove redundant code

--- src/components/config/shared/numeric.js
+++
@@ -1,7 +0,0 @@
-export const parseAsIntOrNothing = (value = '') => {
-  if (value.includes('.')) return undefined;
-  if (value.length === 0) return 0;
-
-  const numeric = parseInt(value);
-  return isNaN(numeric) ? undefined : numeric;
-};
