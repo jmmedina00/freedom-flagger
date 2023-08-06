eb3410cce40436c58523d03edd9d803731855307
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2023-08-06 13:19:38 +0200

    chore(config): add focus trap to modals

--- src/components/config/shared/modal/ModalTemplate.spec.js
+++ src/components/config/shared/modal/ModalTemplate.spec.js
@@ -1,11 +1,28 @@
 import { render } from '@testing-library/vue';
-import { describe, expect } from 'vitest';
+import { afterEach, describe, expect, test, vi } from 'vitest';
 import ModalTemplate from './ModalTemplate.vue';
 import { MODAL_ACTIVE } from '@app/state';
-import { ref } from 'vue';
+import { nextTick, ref } from 'vue';
+import { useFocusTrap } from '@vueuse/integrations/useFocusTrap';
+
+vi.mock('@vueuse/integrations/useFocusTrap');
 
 describe('ModalTemplate', () => {
+  const getMockedTrap = () => {
+    const activate = vi.fn();
+    const deactivate = vi.fn();
+
+    useFocusTrap.mockReturnValue({ activate, deactivate });
+    return { activate, deactivate };
+  };
+
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
   test('should not render anything if injected MODAL_ACTIVE is set to false', () => {
+    getMockedTrap();
+
     const { container } = render(ModalTemplate, {
       global: { provide: { [MODAL_ACTIVE]: ref(false) } },
     });
@@ -15,6 +32,7 @@ describe('ModalTemplate', () => {
   });
 
   test('should render a background that triggers modal closing when clicked', () => {
+    getMockedTrap();
     const active = ref(true);
 
     const { container } = render(ModalTemplate, {
@@ -29,6 +47,8 @@ describe('ModalTemplate', () => {
   });
 
   test('should render slot withing modal container as-is', () => {
+    getMockedTrap();
+
     const { container } = render(ModalTemplate, {
       global: { provide: { [MODAL_ACTIVE]: ref(true) } },
       slots: { default: '<p>This is a test</p>' },
@@ -36,5 +56,35 @@ describe('ModalTemplate', () => {
 
     const content = container.querySelector('.modal p');
     expect(content.innerHTML).toEqual('This is a test');
+  });
+
+  test('should enable focus trap when modal becomes active', async () => {
+    const { activate, deactivate } = getMockedTrap();
+    const active = ref(false);
+
+    const { container } = render(ModalTemplate, {
+      global: { provide: { [MODAL_ACTIVE]: active } },
+    });
+
+    active.value = true;
+    await new Promise((resolve) => setImmediate(() => resolve())); // Force test to wait for nextTick
+
+    expect(activate).toHaveBeenCalled();
+    expect(deactivate).not.toHaveBeenCalled();
+  });
+
+  test('should disable focus trap when modal becomes inactive', async () => {
+    const { activate, deactivate } = getMockedTrap();
+    const active = ref(true);
+
+    const { container } = render(ModalTemplate, {
+      global: { provide: { [MODAL_ACTIVE]: active } },
+    });
+
+    active.value = false;
+    await Promise.resolve();
+
+    expect(deactivate).toHaveBeenCalled();
+    expect(activate).not.toHaveBeenCalled();
   });
 });
--- src/components/config/shared/modal/ModalTemplate.vue
+++ src/components/config/shared/modal/ModalTemplate.vue
@@ -1,15 +1,29 @@
 <script setup>
-  import { inject } from 'vue';
+  import { inject, nextTick, ref, watch } from 'vue';
   import { MODAL_ACTIVE } from '@app/state';
+  import { useFocusTrap } from '@vueuse/integrations/useFocusTrap';
+
+  const div = ref(null);
+
   const active = inject(MODAL_ACTIVE);
+  const { activate, deactivate } = useFocusTrap(div);
 
   const close = () => {
     active.value = false;
   };
+
+  watch(active, async (current) => {
+    if (current) {
+      await nextTick();
+      activate();
+    } else {
+      deactivate();
+    }
+  });
 </script>
 
 <template>
-  <div class="modal is-active" v-if="active">
+  <div class="modal is-active" v-if="active" ref="div">
     <div class="modal-background" @click="close"></div>
     <slot></slot>
   </div>
