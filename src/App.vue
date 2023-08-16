c0f75a30caa32130ba3201536d434b8bbbc3b550
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2023-08-16 16:57:44 +0200

    build: provide default value for state sizing - prevents 1st time crash

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,15 @@
   import ConfigPanel from './components/config/ConfigPanel.vue';
   import RenderPanel from './components/render/RenderPanel.vue';
   import { useFullscreen, useScreenOrientation } from '@vueuse/core';
-  import { FULL_FLAG_DISPLAY } from './state';
+  import { CONFIG_SIZING, FULL_FLAG_DISPLAY } from './state';
   import NotificationBlock from './components/notification/NotificationBlock.vue';
+  import { useSomeConfig } from './components/config/options/plugin';
 
   const fullFlagDisplay = ref(false);
   provide(FULL_FLAG_DISPLAY, fullFlagDisplay);
+
+  // Provide default for first-time boot
+  useSomeConfig(CONFIG_SIZING, { width: 300, aspectRatio: { x: 3, y: 2 } });
 
   const { isSupported, lockOrientation, unlockOrientation } =
     useScreenOrientation();
