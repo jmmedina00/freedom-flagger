64135ca10fa9f8301eb14e254ffd6f537a8ab5b9
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2023-08-19 15:16:25 +0200

    refactor(shared): simplify poly calculation in border decorate

--- src/components/shared/decorate/BorderDecorate.vue
+++ src/components/shared/decorate/BorderDecorate.vue
@@ -2,6 +2,7 @@
   import { DECORATE_CONFIG, DECORATE_SIZE } from '@app/state';
   import { computed, inject, ref } from 'vue';
   import { getViewboxForSizing } from './viewbox';
+  import { directions } from './corner';
 
   const config = inject(DECORATE_CONFIG, ref({}));
   const sizing = inject(DECORATE_SIZE, ref({ width: 0, height: 0 }));
@@ -17,77 +18,45 @@
     ...config.value,
   }));
 
-  const size = computed(() => completedConfig.value.size);
-  const width = computed(() => sizing.value.width);
-  const height = computed(() => sizing.value.height);
-
   const makePoint = (x, y) => `${x},${y}`;
 
-  const cornerPoints = computed(() => {
-    const vWidth = width.value;
-    const vHeight = height.value;
-
-    return [
-      '0,0',
-      makePoint(vWidth, 0),
-      makePoint(vWidth, vHeight),
-      makePoint(0, vHeight),
-    ];
-  });
-
-  const primePoints = computed(() => {
-    const vSize = size.value;
-    const vWidth = width.value;
-    const vHeight = height.value;
-
-    return [
-      makePoint(vSize, vSize),
-      makePoint(vWidth - vSize, vSize),
-      makePoint(vWidth - vSize, vHeight - vSize),
-      makePoint(vSize, vHeight - vSize),
-    ];
-  });
-
   const polygons = computed(() => {
-    const points = cornerPoints.value;
-    const primes = primePoints.value;
+    const borderDirections = ['top', 'right', 'bottom', 'left'];
+    const { size, ...colors } = completedConfig.value;
+    const { width, height } = sizing.value;
+    const maxes = [width, height];
 
-    return new Array(4).fill('0').map((_, corner) => {
-      const secondCorner = (corner + 1) % points.length;
-
-      const verts = [
-        points[corner],
-        points[secondCorner],
-        primes[secondCorner],
-        primes[corner],
-      ];
-
-      return verts.join(' ');
+    const corners = directions.map((pointers) => {
+      const params = pointers.map((p, index) => (p > 0 ? 0 : maxes[index]));
+      return makePoint(...params);
     });
+
+    const primes = directions.map((pointers) => {
+      const params = pointers.map((p, index) =>
+        p > 0 ? size : maxes[index] - size
+      );
+      return makePoint(...params);
+    });
+
+    return borderDirections
+      .map((direction, corner) => {
+        const secondCorner = (corner + 1) % corners.length;
+
+        const verts = [
+          corners[corner],
+          corners[secondCorner],
+          primes[secondCorner],
+          primes[corner],
+        ];
+
+        return { points: verts.join(' '), fill: colors[direction] };
+      })
+      .filter(({ fill }) => !!fill);
   });
 </script>
 
 <template>
   <svg v-bind="viewBox">
-    <polygon
-      v-if="completedConfig.top"
-      :fill="completedConfig.top"
-      :points="polygons[0]"
-    ></polygon>
-    <polygon
-      v-if="completedConfig.right"
-      :fill="completedConfig.right"
-      :points="polygons[1]"
-    ></polygon>
-    <polygon
-      v-if="completedConfig.bottom"
-      :fill="completedConfig.bottom"
-      :points="polygons[2]"
-    ></polygon>
-    <polygon
-      v-if="completedConfig.left"
-      :fill="completedConfig.left"
-      :points="polygons[3]"
-    ></polygon>
+    <polygon v-for="poly in polygons" v-bind="poly"></polygon>
   </svg>
 </template>
