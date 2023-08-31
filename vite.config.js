ae3662f48a70a288bc244d8a2009d4fc3e4f7a8b
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2023-08-31 11:49:01 +0200

    build: add manifest and all PWA shenanigans

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VitePWA({ registerType: 'autoUpdate' })],
   resolve: {
     alias: [{ find: '@app', replacement: resolve(__dirname, 'src') }],
   },
